12b5374f230152dc29975a656485e238fbc280d5
Thaddeus Diamond <thaddeusdiamond@Thaddeuss-MacBook-Pro.local>
2011-07-31 01:10:34 -0700

    Facebook debug

--- public/javascripts/pages/index.js
+++ public/javascripts/pages/index.js
@@ -1,15 +1,16 @@
 Bluebook.FBLogin = function() {
   FB.login(function(response) {
-   if (response.authResponse) {
-     alert('Welcome!  Fetching your information.... ');
-     FB.api('/me', function(response) {
-       alert('Good to see you, ' + response.name + '.');
-       FB.logout(function(response) {
-         alert('Logged out.');
+    alert(response);
+     if (response.authResponse) {
+       alert('Welcome!  Fetching your information.... ');
+       FB.api('/me', function(response) {
+         alert('Good to see you, ' + response.name + '.');
+         FB.logout(function(response) {
+           alert('Logged out.');
+         });
        });
-     });
-   } else {
-     alert('User cancelled login or did not fully authorize.');
-   }
- }, {scope: 'email'});
+     } else {
+       alert('User cancelled login or did not fully authorize.');
+     }
+   }, {scope: 'email'});
 };
