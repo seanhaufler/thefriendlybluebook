7bf5b96e3118d654240489a306599269450aa755
Thaddeus Diamond <thaddeusdiamond@Thaddeuss-MacBook-Pro.local>
2011-07-31 00:54:55 -0700

    Facebook integration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :check_cookie
+
+  def check_cookie
+    # if we have a valid cookie
+    redirect_to search_path and return
+    # else
+    redirect_to root_path and return
+  end
+
   # Design considerations
   $STANDARD_STYLESHEETS = ["bluebook/classes", "bluebook/colors", 
     "bluebook/elements", "bluebook/typography", "gs/gs", "gs/reset", "gs/text",
