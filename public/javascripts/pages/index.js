edffab4577fd00c2017d777903f6e4062b4e6c6f
Thaddeus Diamond <thaddeus.diamond@yale.edu>
2011-07-31 01:25:38 -0700

    Facebook debug

--- public/javascripts/pages/index.js
+++ public/javascripts/pages/index.js
@@ -1,12 +1,8 @@
+//FBLogin(): Log the user in through Facebook
 Bluebook.FBLogin = function() {
     FB.login(function(response) {
         if (response.session && response.session.sig) {
-            alert('Welcome!  Fetching your information.... ');
-            FB.api('/me', function(response) {
-                alert('Good to see you, ' + JSON.stringify(response) + '.');
-            });
-         } else {
-           alert('User cancelled login or did not fully authorize.');
-         }
+            window.location.assign("/search");
+        }
      }, {scope: 'email'});
 };
