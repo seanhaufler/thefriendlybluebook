685e57e1c1e24cd6f1a7e79f3c8140dbe1aae8da
Thaddeus Diamond <thaddeus.diamond@yale.edu>
2011-07-31 21:22:40 -0700

    Queuing off updates for users

--- app/controllers/bluebook_controller.rb
+++ app/controllers/bluebook_controller.rb
@@ -6,12 +6,26 @@ class BluebookController < ApplicationController
   @params: none
   @path: /
   @before_filter: none
+  @method: GET
 
   The following renders the homepage for our application
 =end
   def index
     @stylesheets = ["pages/index"]
     @javascripts = ["pages/index"]
+  end
+
+=begin
+  @params: uid, name, email
+  @path: /update
+  @before_filter: none
+  @method: POST
+
+  The following updates a user's information in the db
+=end
+  def update
+    user = User.find_or_create_by_facebook_id(params[:uid])
+    user.update_attributes({:name => params[:name], :email => params[:email]})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Bluebook::Application.routes.draw do
 
   match "/search", :to => "Search#index"
 
+  match "/update", :to => "Bluebook#update", :via => :post
+
   # Send the user home!
   root :to => "Bluebook#index"
 end
