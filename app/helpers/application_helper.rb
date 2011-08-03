34c2470b70f0e0f54eeb8ded942ed8fb8112c3af
Thaddeus Diamond <thaddeus.diamond@yale.edu>
2011-08-03 00:03:57 -0700

    Bug fix in JSON writing apostrophes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -85,7 +85,7 @@ module ApplicationHelper
   def to_JSON(object)
     json = "{ "
     object.attributes.each do |key, val|
-      json = json + "'#{key}': '#{val}', "
+      json = json + "'#{key}': '#{val.gsub("'", "\\\\'")}', "
     end
     json = json + " }"
   end
