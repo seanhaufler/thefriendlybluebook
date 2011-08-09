d486da4fd29af23a4a2a6743b289fbae81a90bae
Thaddeus Diamond <thaddeus.diamond@yale.edu>
2011-08-08 22:20:29 -0400

    Fixed faking in local data

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -11,8 +11,6 @@ class CoursesController < ApplicationController
   The following adds a comment to the specified course
 =end
   def comment
-    @user = User.find(1)
-    
     # Get the course, push a new comment, and save the course
     course = Course.find(params[:course])
     course.comments.insert(0, [@user.facebook_id, @user.name, params[:content],
